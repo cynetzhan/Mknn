df0f71db3e16134d0a628612e344885038350ff8
palapajr <qumfaanzirr@gmail.com>
2021-01-18 22:03:52 +0700

    ada perubahan di view dashboard bg

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,52 +1,61 @@
+<?php if ($this->session->userdata('nama')) { ?>
+
     <br><h1 class="heading text-center">SISTEM PEMILIHAN JURUSAN SMA NEGERI 6 KOTA PEKANBARU</h1>
-<!-- Page Content -->
-<div class="content">
-    <div class="row invisible" data-toggle="appear">
+    <!-- Page Content -->
+    <div class="content">
+        <div class="row invisible" data-toggle="appear">
 
-        <div class="col-6 col-xl-3">
-            <a class="block block-rounded block-bordered block-link-shadow" href="javascript:void(0)">
-                <div class="block-content block-content-full clearfix">
-                    <div class="float-right mt-15 d-none d-sm-block">
-                        <i class="si si-users fa-2x text-primary-light"></i>
+            <div class="col-6 col-xl-3">
+                <a class="block block-rounded block-bordered block-link-shadow" href="javascript:void(0)">
+                    <div class="block-content block-content-full clearfix">
+                        <div class="float-right mt-15 d-none d-sm-block">
+                            <i class="si si-users fa-2x text-primary-light"></i>
+                        </div>
+                        <div class="font-size-h3 font-w600 text-primary"><?php echo $total_asset; ?></div>
+                        <div class="font-size-sm font-w600 text-uppercase text-muted">Data Siswa Training</div>
                     </div>
-                    <div class="font-size-h3 font-w600 text-primary"><?php echo $total_asset; ?></div>
-                    <div class="font-size-sm font-w600 text-uppercase text-muted">Data Siswa Training</div>
-                </div>
-            </a>
-        </div>
-        <div class="col-6 col-xl-3">
-            <a class="block block-rounded block-bordered block-link-shadow" href="javascript:void(0)">
-                <div class="block-content block-content-full clearfix">
-                    <div class="float-right mt-15 d-none d-sm-block">
-                        <i class="si si-users fa-2x text-earth-light"></i>
+                </a>
+            </div>
+            <div class="col-6 col-xl-3">
+                <a class="block block-rounded block-bordered block-link-shadow" href="javascript:void(0)">
+                    <div class="block-content block-content-full clearfix">
+                        <div class="float-right mt-15 d-none d-sm-block">
+                            <i class="si si-users fa-2x text-earth-light"></i>
+                        </div>
+                        <div class="font-size-h3 font-w600 text-earth"><?php echo $test; ?></div>
+                        <div class="font-size-sm font-w600 text-uppercase text-muted">Data Siswa Testing</div>
                     </div>
-                    <div class="font-size-h3 font-w600 text-earth"><?php echo $test; ?></div>
-                    <div class="font-size-sm font-w600 text-uppercase text-muted">Data Siswa Testing</div>
-                </div>
-            </a>
-        </div>
-    </div>  
-
-    <!-- <div class="block">
-        <div class="block-content">
-            <div class="row items-push">
-                <div class="col-md-4">
-                <div class="block-content p-0 overflow-hidden">
-                    <a class="img-link" href="">
-                        <img class="img-fluid rounded-top" src="<?= base_url() ?>assets/media/logo.jpg" alt="">
-                    </a>
-                </div>
-
-                </div>
-                <div class="col-md-8 text-center">
-                    <h1>Sistem Pemilihan Jurusan <br> SMA Negeri 6 Kota Pekanbaru. </h1>
-                    <h4><small>Decision Support System Modified K-Nearest Neighbor </small></h4>
-                    <hr>
-                    
-                    
-                </div>
+                </a>
             </div>
         </div>
-    </div> -->
-</div>
-<!-- END Page Content -->
+    </div>
+    <!-- END Page Content -->
+
+    <?php } else { ?>
+    <div class="content">
+        <h1 class="heading text-center">SISTEM PEMILIHAN JURUSAN SMA NEGERI 6 KOTA PEKANBARU</h1>
+            <div class="block">
+                <div class="block-content">
+                    <div class="row items-push">
+                        <div class="col-md-4">
+                        <div class="block-content p-0 overflow-hidden">
+                            <a class="img-link" href="">
+                                <img class="img-fluid rounded-top" src="<?= base_url() ?>assets/media/logo.jpg" alt="">
+                            </a>
+                        </div>
+
+                        </div>
+                        <div class="col-md-8 text-center">
+                            <h1>Sistem Pemilihan Jurusan <br> SMA Negeri 6 Kota Pekanbaru. </h1>
+                            <h4><small>Decision Support System Modified K-Nearest Neighbor </small></h4>
+                            <hr>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+<?php } ?>
+   
+   
